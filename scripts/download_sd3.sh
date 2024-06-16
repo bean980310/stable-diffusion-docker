2a99622e71028f47383c8b283ebf10b2cffae5dd
bean980310 <bean980310@hotmail.co.kr>
2024-06-16 16:47:59 +0900

    file updated

--- scripts/download_sd3.sh
+++ scripts/download_sd3.sh
@@ -1,6 +1,10 @@
 huggingface-cli login
 
-huggingface-cli download stabilityai/stable-diffusion-3-medium sd3_medium.safetensors --local-dir=/workspace/stable-diffusion-webui/models/Stable-diffusion
-huggingface-cli download stabilityai/stable-diffusion-3-medium sd3_medium_incl_clips.safetensors --local-dir=/workspace/stable-diffusion-webui/models/Stable-diffusion
-huggingface-cli download stabilityai/stable-diffusion-3-medium sd3_medium_incl_clips_t5xxlfp8.safetensors --local-dir=/workspace/stable-diffusion-webui/models/Stable-diffusion
-huggingface-cli download stabilityai/stable-diffusion-3-medium sd3_medium_incl_clips_t5xxlfp16.safetensors --local-dir=/workspace/stable-diffusion-webui/models/Stable-diffusion
+echo "Downloading SD 3 model"
+
+cd /workspace/stable-diffusion-webui/models/Stable-diffusion
+
+huggingface-cli download stabilityai/stable-diffusion-3-medium sd3_medium.safetensors --local-dir=./models/Stable-diffusion
+huggingface-cli download stabilityai/stable-diffusion-3-medium sd3_medium_incl_clips.safetensors --local-dir=./models/Stable-diffusion
+huggingface-cli download stabilityai/stable-diffusion-3-medium sd3_medium_incl_clips_t5xxlfp8.safetensors --local-dir=./models/Stable-diffusion
+huggingface-cli download stabilityai/stable-diffusion-3-medium sd3_medium_incl_clips_t5xxlfp16.safetensors --local-dir=./models/Stable-diffusion
